15c5522ee01b538fe490826ec07ed9184369185b
JaeSung Choi <ky4171@naver.com>
2022-03-18 16:05:45 +0900

    http

---
+++ src/main/java/project/firstServlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,30 @@
+package project.firstServlet.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import project.firstServlet.basic.HelloData;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "responseJsonServlet",urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("utf-8");
+
+        HelloData helloData = new HelloData();
+        helloData.setUsername("JESS");
+        helloData.setAge(26);
+
+        String result = objectMapper.writeValueAsString(helloData);
+        resp.getWriter().write(result);
+    }
+}
