d105f5d4cc5e7aae8c675c06e1ba30f51923c711
JaeSung Choi <ky4171@naver.com>
2022-03-21 17:51:06 +0900

    SpringMvc

---
+++ src/main/java/project/firstServlet/web/springmvc/v1/SpringMemberSaveControllerV1.java
@@ -0,0 +1,27 @@
+package project.firstServlet.web.springmvc.v1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import project.firstServlet.domain.member.Member;
+import project.firstServlet.domain.member.MemberRepository;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class SpringMemberSaveControllerV1 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members/save")
+    public ModelAndView process(HttpServletRequest req, HttpServletResponse resp) {
+        String username = req.getParameter("username");
+        int age = Integer.parseInt(req.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        ModelAndView mv = new ModelAndView("save-result");
+        mv.addObject("member",member);
+        return mv;
+    }
+}
